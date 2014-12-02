ebd0f3a9a25a8e22aad03d906b711b2b1b2dae70
John <johnjmartiniv@gmail.com>
2014-12-01 20:07:59 -0500

    Cool main shit.

---
+++ src/main.java
@@ -0,0 +1,15 @@
+// Assignment 10
+// Martin John
+// jmartin4
+// Ben Campbell
+// campb
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+
+import tester.*;
+import javalib.funworld.*;
+import javalib.worldimages.*;
+import javalib.colors.*;
+import javalib.worldcanvas.*;
