37889aaa9ac8383b3e9dff9fd8c58034fbbc56fe
John <johnjmartiniv@gmail.com>
2014-12-03 11:11:58 -0500

    with weight.

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@
 // Martin John
 // jmartin4
 // Ben Campbell
-// campb
+// nampb
 
 import java.awt.Color;
 import java.util.ArrayList;
@@ -21,19 +21,17 @@ class Node {
     // screen
     int x;
     int y;
+    int halfWeight;
     // the four adjacent cells to this one
-    Edge left;
-    Edge top;
-    Edge right;
-    Edge bottom;
+    Node left;
+    Node top;
+    Node right;
+    Node bottom;
     
-    Node(int x, int y) {
+    Node(int x, int y, int weight) {
         this.x = x;
         this.y = y;
-        this.left = new NullEdge(this);
-        this.top = new NullEdge(this);
-        this.right = new NullEdge(this);
-        this.bottom = new NullEdge(this);
+        this.halfWeight = weight;
     }
     
 }
@@ -70,12 +68,13 @@ class MazeGame {
     ArrayList<ArrayList<Node>> buildMatrix() {
         ArrayList<ArrayList<Node>> result =
                 new ArrayList<ArrayList<Node>>();
+        Random rand = new Random();
         
         for (int indexY = 0; indexY < BOARD_HEIGHT; indexY = indexY + 1) {
             ArrayList<Node> tempX = new ArrayList<Node>();
 
             for (int indexX = 0; indexX < BOARD_WIDTH; indexX = indexX + 1) {
-                tempX.add(new Node(indexX, indexY));   
+                tempX.add(new Node(indexX, indexY, rand.nextInt(100)));
             }
             
             result.add(tempX);
@@ -85,16 +84,51 @@ class MazeGame {
     }
     
     ArrayList<ArrayList<Node>> massageMatrix(ArrayList<ArrayList<Node>>
-        before) {
+        matrix) {
         
-        for (int indexY = 0; indexY < BOARD_HEIGHT; indexY = indexY + 1) {
-            ArrayList<Node> tempX = new ArrayList<Node>();
+        for (ArrayList<Node> arr : matrix) {
+            for (Node n : arr) {
+                // Y-Axis
+                if (n.y == 0) {
+                    n.top = n;
+                    n.bottom = matrix.get(n.y + 1).get(n.x);
+                }
+                else if (n.y == BOARD_HEIGHT - 1) {
+                    n.top = matrix.get(n.y - 1).get(n.x);
+                    n.bottom = n;
+                }
+                else {
+                    n.top = matrix.get(n.y - 1).get(n.x);
+                    n.bottom = matrix.get(n.y + 1).get(n.x);
+                }
 
-            for (int indexX = 0; indexX < BOARD_WIDTH; indexX = indexX + 1) {
-                tempX.add(new Node(indexX, indexY));   
+                // X-Axis
+                if (n.x == 0) {
+                    n.left = n;
+                    n.right = matrix.get(n.y).get(n.x + 1);
+                }
+                else if (n.x == BOARD_WIDTH - 1) {
+                    n.left = matrix.get(n.y).get(n.x - 1);
+                    n.right = n;
+                }
+                else {
+                    n.left = matrix.get(n.y).get(n.x - 1);
+                    n.right = matrix.get(n.y).get(n.x + 1);
+                }
             }
-            
-            result.add(tempX);
+        }
+        
+        return matrix;
+    }
+    
+    ArrayList<Edge> extractEdges(ArrayList<ArrayList<Node>>
+        matrix) {
+        
+        for (ArrayList<Node> arr : matrix) {
+            for (Node n : arr) {
+                
+            }
         }
     }
+    
 }
