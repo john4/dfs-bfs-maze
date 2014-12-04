770144326cc27a7dcd2d233d82986ce407cfef49
John <johnjmartiniv@gmail.com>
2014-12-04 17:58:46 -0500

    union-find and kruskals, yo.

--- src/main.java
+++ src/main.java
@@ -6,6 +6,8 @@
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
 import tester.*;
@@ -21,19 +23,20 @@ class Node {
     // screen
     int x;
     int y;
-    int halfWeight;
     // the four adjacent cells to this one
     Node left;
     Node top;
     Node right;
     Node bottom;
     
-    Node(int x, int y, int weight) {
+    Node(int x, int y) {
         this.x = x;
         this.y = y;
-        this.halfWeight = weight;
     }
     
+    public String toString() {
+        return "(" + Integer.toString(this.x) + ", " + Integer.toString(this.y) + ")";
+    }
 }
 
 class Edge {
@@ -50,32 +53,38 @@ class Edge {
 
 class MazeGame {
     // Constants
-    static final int BOARD_WIDTH = 64; // in Nodes
-    static final int BOARD_HEIGHT = 64; // in Nodes
+    static final int BOARD_WIDTH = 100; // in Nodes
+    static final int BOARD_HEIGHT = 60; // in Nodes
     static final int NODE_SIZE = 20; // in px
     
     ArrayList<ArrayList<Node>> map;
+    ArrayList<Edge> edges;
     
     MazeGame() {
-        
+        this.initMaze();
     }
     
     void initMaze() {
+        Utils utils = new Utils();
+        
         ArrayList<ArrayList<Node>> tempMatrix = this.buildMatrix();
         tempMatrix = this.massageMatrix(tempMatrix);
         ArrayList<Edge> edges = this.extractEdges(tempMatrix);
+        utils.sort(edges);
+        
+        this.edges = edges;
+        this.map = tempMatrix;
     }
     
     ArrayList<ArrayList<Node>> buildMatrix() {
         ArrayList<ArrayList<Node>> result =
                 new ArrayList<ArrayList<Node>>();
-        Random rand = new Random();
         
         for (int indexY = 0; indexY < BOARD_HEIGHT; indexY = indexY + 1) {
             ArrayList<Node> tempX = new ArrayList<Node>();
 
             for (int indexX = 0; indexX < BOARD_WIDTH; indexX = indexX + 1) {
-                tempX.add(new Node(indexX, indexY, rand.nextInt(100)));
+                tempX.add(new Node(indexX, indexY));
             }
             
             result.add(tempX);
@@ -126,23 +135,142 @@ class MazeGame {
     // Returns a list of the Edges as implied by the connections in the given matrix of Nodes.
     ArrayList<Edge> extractEdges(ArrayList<ArrayList<Node>>
         matrix) {
-        
+
+        Random rand = new Random();
         ArrayList<Edge> result = new ArrayList<Edge>();
         
         for (ArrayList<Node> arr : matrix) {
             for (Node n : arr) {
-                if(n.x != BOARD_WIDTH) {
+                if(n.x != BOARD_WIDTH - 1) {
                     // Right
-                    result.add(new Edge(n, n.right, n.halfWeight + n.right.halfWeight));
+                    result.add(new Edge(n, n.right, rand.nextInt(100)));
                 }
-                if(n.y != BOARD_HEIGHT) {
+                if(n.y != BOARD_HEIGHT - 1) {
                     // Bottom
-                    result.add(new Edge(n, n.bottom, n.halfWeight + n.bottom.halfWeight));
+                    result.add(new Edge(n, n.bottom, rand.nextInt(100)));
                 }
             }
         }
         
         return result;
     }
+    
+    ArrayList<Edge> kruskal(ArrayList<Edge> edges, ArrayList<ArrayList<Node>> nodes) {
+        HashMap<String, String> reps = new HashMap<String, String>();
+        ArrayList<Edge> edgesInTree = new ArrayList<Edge>();
+        Utils utils = new Utils();
+        
+        // Init all representations to themselves.
+        for(ArrayList<Node> arr : nodes) {
+            for(Node n : arr) {
+                reps.put(n.toString(), n.toString());
+            }
+        }
+        
+        // 
+        
+        for (int index = 1 ; index < edges.size(); index = index + 1) {
+            
+            if(utils.find(reps, edges.get(index).a.toString()).equals(utils.find(reps, (edges.get(index).b.toString())))) {
+                
+            }
+            else {
+                edgesInTree.add(edges.get(index));
+                utils.union(reps, utils.find(reps, edges.get(index).a.toString()), 
+                        utils.find(reps, edges.get(index).b.toString()));
+            }
+        }
+        
+        return edgesInTree;
+    }
+}
+
+class Utils {
+    //Effect swap the values at 2 indices in given array
+    <T> void swap(ArrayList<T> arr, int idx1, int idx2) {
+        T temp = arr.get(idx1);
+        arr.set(idx1, arr.get(idx2));
+        arr.set(idx2, temp);
+        
+    }
+        
+    // Finds the index of the minimum item after the given start
+    // Assumes that start is in the list
+    int findMin(ArrayList<Edge> arr, int start) {
+        Edge minSoFar = arr.get(start);
+        int indexOfMin = start;
+        for (int index = start; index < arr.size(); index = index + 1) {
+            if(minSoFar.weight >= arr.get(index).weight) {
+                minSoFar = arr.get(index);
+                indexOfMin = index;
+            }
+        }
+        return indexOfMin;
+    }
+    
+    void sort(ArrayList<Edge> arr) {
+        for(int index = 0; index < arr.size(); index = index + 1) {
+            int indexOfMin = this.findMin(arr, index);
+            this.swap(arr, indexOfMin, index);
+        }
+    }
+    
+    String find(HashMap<String, String> hashMap, String key) {
+        String k = hashMap.get(key);
+        
+        if(k.equals(key)) {
+            return k;
+        }
+        else {
+            return 
+                    this.find(hashMap, k);
+        }
+    }
+    
+    void union(HashMap<String, String> hashMap, String rep1, String rep2) {
+        hashMap.put(rep1, rep2);
+        
+    }
+}
+
+class ExamplesMaze {
+    MazeGame game;
+    
+    void setup() {
+        this.game = new MazeGame();
+    }
+    
+    void testConstructor(Tester t) {
+        this.setup();
+        t.checkExpect(this.game.edges.size(), 11840);
+    }
+    
+    void testKuskal(Tester t) {
+        this.setup();
+        t.checkExpect(this.game.kruskal(this.game.edges, this.game.map).size(), 5999);
+    }
+    
+    void testUtils(Tester t) {
+       ArrayList<Edge> test = new ArrayList<Edge>(Arrays.asList(new Edge(new Node(1, 1), new Node(1, 1), 10), 
+               new Edge(new Node(1, 1), new Node(1, 1), 9), new Edge(new Node(1, 1), new Node(1, 1), 5),
+               new Edge(new Node(1, 1), new Node(1, 1), 15)));
+       Utils utils = new Utils();
+       utils.sort(test);
+       
+       HashMap<String, String> hash = new HashMap();
+       hash.put("A", "A");
+       hash.put("B", "B");
+       hash.put("C", "C");
+       hash.put("D", "E");
+       hash.put("E", "A");
+       
+       t.checkExpect(utils.find(hash, "D"), "A");
+       t.checkExpect(utils.find(hash, "E"), "A");
+       t.checkExpect(utils.find(hash, "A"), "A");
+    }
+    
+    
+    
+    
     
 }
